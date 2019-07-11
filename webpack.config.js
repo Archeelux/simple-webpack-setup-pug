818edfecf12e349e1f4ef06e1783816e80b80f11
Archeelux <archeelux@gmail.com>
2019-07-11 17:42:14 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+    entry: {
+        app: "./src/app.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js"
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.pug"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                use: ["pug-loader"]
+            }
+        ]
+    }
+};
+module.exports = (env, argv) => {
+    if (argv.mode === "development") {
+    }
+    if (argv.mode === "production") {
+    }
+    return config;
+};
