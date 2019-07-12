e09c7b527b4c86efd0100ce5f4eaeb1a0940fb66
Archeelux <archeelux@gmail.com>
2019-07-12 10:01:06 +0100

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
     entry: {
@@ -17,12 +16,6 @@ const config = {
         new HtmlWebpackPlugin({
             template: "./src/index.pug"
         })
-        // new MiniCssExtractPlugin({
-        //     // Options similar to the same options in webpackOptions.output
-        //     // both options are optional
-        //     filename: "bundle.css",
-        //     chunkFilename: "[id].css"
-        // })
     ],
     module: {
         rules: [
@@ -32,23 +25,7 @@ const config = {
             },
             {
                 test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                    // {
-                    //     loader: MiniCssExtractPlugin.loader,
-                    //     options: {
-                    //         hmr: process.env.NODE_ENV === "development"
-                    //     }
-                    // },
-                    // {
-                    //     loader: "css-loader"
-                    // },
-                    // {
-                    //     loader: "fast-sass-loader"
-                    // }
-                ]
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     }
